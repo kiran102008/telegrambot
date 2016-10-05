b068840a3d8502719cb2e7a0a92416dbe3117a5a
kiran102008 <kraju49@gmail.com>
2016-10-05 22:08:06 +0530

    Update hook.php

--- examples/hook.php
+++ examples/hook.php
@@ -12,8 +12,8 @@
 require __DIR__ . '/vendor/autoload.php';
 
 // Add you bot's API key and name
-$API_KEY = 'your_bot_api_key';
-$BOT_NAME = 'username_bot';
+$API_KEY = '278835567:AAHFjzHG1dd1leHD9NtKYhmOkklYmFyALdk';
+$BOT_NAME = 'kingsen_bot';
 
 // Define a path for your custom commands
 //$commands_path = __DIR__ . '/Commands/';
